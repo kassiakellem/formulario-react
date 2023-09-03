c5466b436d430a0608cc186b7125c87453d08855
Kassiakellem <kassiakellem4@gmail.com>
2023-09-03 16:41:18 -0300

    O que aprendemos nessa aula:
    Como lidar com problemas de assincronicidade
    
    Quando usar o Hook de UseEffect
    
    Ciclo de vida dos componentes funcionais
    
    Como utilizar o componente Stepper do MaterialUI

--- src/Componente/FormularioCadastro/FormularioCadastro.jsx
+++ src/Componente/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega/DadosEntrega";
+import { Step, StepLabel, Stepper, Typography } from "@mui/material";
 
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState("")
+  useEffect(()=>{
+    if(etapaAtual === Formulario.length- 1){
+    aoEnviar(dadosColetados)
+}})
 
-  const Formulario = [<DadosUsuario aoEnviar={Proximo}/>,<DadosPessoais aoEnviar={Proximo} validarCPF={validarCPF} />, <DadosEntrega aoEnviar={aoEnviar}/>]
+  const Formulario = [
+  <DadosUsuario aoEnviar={ColetarDados}/>,
+  <DadosPessoais aoEnviar={ColetarDados} validarCPF={validarCPF} />, 
+  <DadosEntrega aoEnviar={ColetarDados}/>,
+  <Typography variant="h5" margin={5} textAlign={"center"}>Obrigada pelo cadastro!</Typography>
+]
+
+  function ColetarDados(dados){
+    setDados({...dadosColetados, ...dados})
+    Proximo();
+    
+  }
 
   function Proximo() {
  setEtapaAtual(etapaAtual+1);
@@ -17,7 +34,12 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
 
   return (
     <>
-      
+   <Stepper activeStep={etapaAtual}>
+      <Step><StepLabel>Login</StepLabel></Step>
+      <Step><StepLabel>Pessoal</StepLabel></Step>
+      <Step><StepLabel>Entrega</StepLabel></Step>
+      <Step><StepLabel>Finalização</StepLabel></Step>
+    </Stepper>
       {Formulario [etapaAtual]};
       
     </>
